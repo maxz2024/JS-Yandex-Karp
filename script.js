c30f223eefd1112f7faff1d7235c56015e211eb5
Домовой <143743445+maxz2024@users.noreply.github.com>
2023-12-19 15:44:31 +0000

    Add check null input

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const container = document.getElementById("heroesContainer");
+let button = document.getElementById("addButton");
+let nameInput = document.getElementById("heroName");
+let classInput = document.getElementById("heroClass");
 
 let heroes = [["Axe", "Tank"], ["Crystal Maiden", "Support"]];
 
-function displayHeroes()  {
+function displayHeroes() {
 
     let heroesContainer = document.getElementById("heroesContainer");
 
 
-    heroesContainer.innerHTML = ''; 
-    
+    heroesContainer.innerHTML = '';
+
     for (let i = 0; i < heroes.length; i++) {
         let heroDiv = document.createElement("div");
+
+        heroDiv.className = "heroCard"
 
         heroDiv.innerHTML = `<h3>${heroes[i][0]}</h3><p>${heroes[i][1]}</p>`;
 
@@ -20,18 +25,39 @@ function displayHeroes()  {
 
 function addHero() {
 
-    let nameInput = document.getElementById("heroName");
-    let classInput = document.getElementById("heroClass");
-    if (nameInput != "" && classInput	 != "") {
-       heroes.push([nameInput.value,classInput.value]);
+    if (nameInput.value != "" && classInput.value != "") {
+        heroes.push([nameInput.value, classInput.value]);
+        displayHeroes();
+
+        nameInput.value = "";
+        classInput.value = "";
     }
-    else { let error = document.createElement("p"); error.innerHTML = "Ошибка"; document.body.appendChild(error); }
+    else {
+        let error = document.getElementById("error");
+        error.innerHTML = "Ошибка. Поля пустые.";
+        button.disabled = true;
+        
+    }
 
-    displayHeroes();
-
-    nameInput.value = "";
-    classInput.value = "";
-} 
+}
 
 displayHeroes();
-document.getElementById("addButton").addEventListener("click", addHero); 
+
+function check(event) {
+    if (nameInput.value != "" && classInput.value != "") {
+        button.disabled = false;
+        let error = document.getElementById("error");
+        error.innerHTML = "";
+    }
+    else {
+        button.disabled = true;
+        let error = document.getElementById("error");
+        error.innerHTML = "Ошибка. Поля пустые.";
+    }
+}
+
+// Хотел написать setTimeout, но это работает не правильно. Подскажите как можно сделать?
+button.onmouseover = check;
+
+document.getElementById("addButton").addEventListener("click", addHero);
+
