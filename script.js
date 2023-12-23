f90120040e9492fcb326607f8daf0adf3d30c7d4
Домовой <143743445+maxz2024@users.noreply.github.com>
2023-12-23 09:13:35 +0000

    edit check inputs

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ function addHero() {
 
         nameInput.value = "";
         classInput.value = "";
+        button.disabled = true;
     }
     else {
         let error = document.getElementById("error");
@@ -48,23 +49,40 @@ function addHero() {
 
 displayHeroes();
 
-function check(event) {
-    if (nameInput.value != "" && classInput.value != "") {
-        button.disabled = false;
-        let error = document.getElementById("error");
-        error.innerHTML = "";
-    }
-    else {
-        button.disabled = true;
-        let error = document.getElementById("error");
-        error.innerHTML = "Ошибка. Поля пустые.";
-    }
-    timer = setTimeout(check,2000);
-    
+let inputs = [].slice.call(document.querySelectorAll('input[type="text"]')) // преобразование NodeList в Array для использования функций массива
+
+inputs.forEach(function(el){
+  el.addEventListener('input', checkInputs, false);
+});
+
+function checkInputs(){
+	let empty = inputs.filter(function(el){
+        // if (el.value.trim() === '') {
+        //     el.style.border = "1px solid red"
+        // }
+        // else if (el.value.trim() !== ''){
+        //     el.style.border = "1px solid green"
+        // }
+    return el.value.trim() === ''; // удаляем лишние пробелы
+  }).length;
+  button.disabled = empty !== 0;
+  
+  if (empty === 0) {
+    let error = document.getElementById("error");
+    error.innerHTML = "";
+  }
 }
+// checkInputs();
 
 button.onmouseover = function () {
-    timer = setTimeout(check,2000);
+    if (nameInput.value.trim() == "" && classInput.value.trim() == "") {
+        timer = setTimeout(
+            function check() {
+                let error = document.getElementById("error");
+                error.innerHTML = "Ошибка. Поля пустые.";
+            },2000); 
+    }
+    // timer = setTimeout(check,2000);
 }
 button.onmouseout = function () {
     clearTimeout(timer)
